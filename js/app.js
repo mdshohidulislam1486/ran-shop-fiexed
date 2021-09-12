a7b58fc3cddcfea738a490ca6e2aeb8a43eaa8c7
mdshohidulislam1486 <Shohidul Islam>
2021-09-13 01:06:37 +0600

    upadated

--- js/app.js
+++ js/app.js
@@ -2,27 +2,32 @@ const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showProducts(data));
-};
+    .then((data) => showProducts(data))
+    .catch(error =>{console.log(error)});
+}; 
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products)
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.classList.add("col");
+    div.innerHTML = `<div class="card bg-secondary h-100">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="card-img-top" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <div class='card-body'>
+        <h3 class='card-title'>${product.title}</h3>
+        <p>Category: ${product.category}</p>
+        <h2>Price: $ ${product.price}</h2>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+        <button id="details-btn" class="btn btn-danger">Details</button></div>
+      </div>
       `;
+
     document.getElementById("all-products").appendChild(div);
   }
 };
@@ -76,5 +81,6 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+    console.log(grandTotal)
+    setInnerText('total', grandTotal);
 };
