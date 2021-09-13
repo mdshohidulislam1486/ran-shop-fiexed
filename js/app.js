c810de08a0cf26f211d465ee7c2b50b46bdecd73
mdshohidulislam1486 <Shohidul Islam>
2021-09-13 22:31:50 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -22,7 +22,8 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <p><span>${product.rating.rate}</span><span>${product.rating.count}</span> </p>
+      <p><div class="Stars" style="--rating:${product.rating.rate};" aria-label="Rating of this product is 2.3 out of 5."></div></p>
+      <p>${product.rating.count} reviews </p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -80,8 +81,8 @@ const updateTaxAndCharge = () => {
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price").toFixed(2) + getInputValue("delivery-charge").toFixed(2) +
-    getInputValue("total-tax").toFixed(2);
+    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("total-tax");
     console.log(grandTotal)
     setInnerText('total', parseFloat(grandTotal).toFixed(2));
 };
